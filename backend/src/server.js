2c95ba4047197a8d92cc6a496b2de07f571b0e83
Jorge Cardoso Rabello <jorgecardosorabello@gmail.com>
2020-04-18 14:13:39 +0200

    pictures become url

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+// declaro o path que é utilizado para caminhos na minha aplicação
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -8,8 +12,12 @@ mongoose.connect('mongodb+srv://aircncjorge:aircncjorge@aircnc-4stg0.mongodb.net
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-
+app.use(cors());
 app.use(express.json());
+/* e aqui vou criar o files com o express.static que é a funcionalidade
+que lida com arquivos estáticos.Algo com upload.
+E dentro do static() passo o caminho para os arquivos, a pasta upload  */
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
